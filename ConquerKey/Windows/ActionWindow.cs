d1072833fa76c4ffa7ea276873f417b106f03ded
Tony Tran <tony.tran@wisetechglobal.com>
2025-07-15 16:56:35 +1000

    Add hint labels

--- ConquerKey/Windows/ActionWindow.cs
+++ ConquerKey/Windows/ActionWindow.cs
@@ -1,7 +1,10 @@
 ﻿using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Automation;
+using System.Windows.Controls;
+using System.Windows.Media;
 using ConquerKey.ActionHandlers;
+using static ConquerKey.WindowUtilities;
 
 namespace ConquerKey.Windows;
 
@@ -20,14 +23,69 @@ public class ActionWindow : Window
 		
 		var foregroundWindow = GetForegroundWindow();
 		_activeWindow = AutomationElement.FromHandle(foregroundWindow);
-		
 		_interactableElements = actionHandler.FindInteractableElements(_activeWindow);
+		
+		ConfigureWindow();
+		AddHintLabels();
+	}
 
+	private void AddHintLabels()
+	{
+		for (var index = 0; index < _interactableElements.Count; index++)
+		{
+			var clickableElement = _interactableElements[index];
+			AddHintLabel(clickableElement, index);
+		}
+		
+		void AddHintLabel(AutomationElement clickableElement, int index)
+		{
+			// Add your logic here
+			var textBlock = new TextBlock
+			{
+				Text = index.ToString(),
+				Foreground = Brushes.Black,
+				FontSize = 12,
+				Background = Brushes.Aqua,
+				Margin = new Thickness(0),
+				Padding = new Thickness(0),
+			};
+
+			var x = PixelToDeviceIndependentUnit(this, clickableElement.Current.BoundingRectangle.X - _activeWindow.Current.BoundingRectangle.X, false);
+			var y = PixelToDeviceIndependentUnit(this, clickableElement.Current.BoundingRectangle.Y - _activeWindow.Current.BoundingRectangle.Y, true);
+
+			// Set the absolute position
+			Canvas.SetLeft(textBlock, x); // X-coordinate
+			Canvas.SetTop(textBlock, y); // Y-coordinate
+
+			// Add the TextBlock to the Canvas
+			if (Content is Canvas canvas)
+			{
+				canvas.Children.Add(textBlock);
+			}
+		}
+	}
+
+	private void ConfigureWindow()
+	{
 		Topmost = true;
-		Width = WindowUtilities.PixelToDeviceIndependentUnit(this, _activeWindow.Current.BoundingRectangle.Width, false);
-		Height = WindowUtilities.PixelToDeviceIndependentUnit(this, _activeWindow.Current.BoundingRectangle.Height + 30,
-			true); // + 30 for textbox to input the hint number
-		Left = WindowUtilities.PixelToDeviceIndependentUnit(this, _activeWindow.Current.BoundingRectangle.Left, false);
-		Top = WindowUtilities.PixelToDeviceIndependentUnit(this, _activeWindow.Current.BoundingRectangle.Top, true);
+		Width = PixelToDeviceIndependentUnit(this, _activeWindow.Current.BoundingRectangle.Width, false);
+		// + 30 for textbox to input the hint number
+		Height = PixelToDeviceIndependentUnit(this, _activeWindow.Current.BoundingRectangle.Height + 30, true);
+		Left = PixelToDeviceIndependentUnit(this, _activeWindow.Current.BoundingRectangle.Left, false);
+		Top = PixelToDeviceIndependentUnit(this, _activeWindow.Current.BoundingRectangle.Top, true);
+		WindowStyle = WindowStyle.None;
+		AllowsTransparency = true;
+		Background = Brushes.Transparent;
+		
+		var canvas = new Canvas();
+
+		var border = new Border
+		{
+			BorderBrush = Brushes.White,
+			BorderThickness = new Thickness(10),
+			CornerRadius = new CornerRadius(5)
+		};
+		canvas.Children.Add(border);
+		Content = canvas;
 	}
 }
